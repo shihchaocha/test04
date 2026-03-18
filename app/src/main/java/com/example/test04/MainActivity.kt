404f4793ec29c12522626c1aa9594e843d03e382
Shi-Cho Cha <csc@cs.ntust.edu.tw>
2026-03-18 15:53:44 +0800

    Commit tips

--- app/src/main/java/com/example/test04/MainActivity.kt
+++ app/src/main/java/com/example/test04/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.example.test04
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.doAfterTextChanged
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val etBillAmount = findViewById<EditText>(R.id.etBillAmount)
+        val tvTipAmount = findViewById<TextView>(R.id.tvTipAmount)
+        val currencyFormatter = NumberFormat.getCurrencyInstance()
+
+        fun updateTipAmount() {
+            val billAmount = etBillAmount.text.toString().trim().toDoubleOrNull() ?: 0.0
+            val tipAmount = billAmount * 0.15
+            tvTipAmount.text = "Tip Amount: ${currencyFormatter.format(tipAmount)}"
+        }
+
+        updateTipAmount()
+        etBillAmount.doAfterTextChanged {
+            updateTipAmount()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
